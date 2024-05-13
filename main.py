2496e0a2b20c01ffe41cd8c3e0b81a8873eacfe3
verblead <147359060+verblead@users.noreply.github.com>
2024-05-13 17:29:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -191,4 +191,4 @@ if __name__ == "__main__":
         print("No leads found or all leads have errors.")
 
     # Start the Flask app
-    app.run(debug=True, host='0.0.0.0', port=5000) #Example port
+    app.run(debug=False, host='0.0.0.0', port=5000, use_reloader=False)  # Disable reloader
